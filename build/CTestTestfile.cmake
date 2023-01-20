7a93ad1f8365ad46baa1e42e22fe210b8585ed2f
roshangeorge97 <roshangeorge2003@gmail.com>
2023-01-20 10:25:19 +0530

    first commit

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/roshangeorge/catkin_workspace/src
+# Build directory: /home/roshangeorge/catkin_workspace/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("ReallyUsefulRobot/ROS/rur_navigation")
+subdirs("ReallyUsefulRobot/ROS/rur_detailed_description")
+subdirs("ReallyUsefulRobot/ROS/rur_gazebo")
+subdirs("ReallyUsefulRobot/ROS/rur_description")
+subdirs("ReallyUsefulRobot/ROS/rur")
---
+++ build/ReallyUsefulRobot/ROS/rur_detailed_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/roshangeorge/catkin_workspace/src/ReallyUsefulRobot/ROS/rur_detailed_description
+# Build directory: /home/roshangeorge/catkin_workspace/build/ReallyUsefulRobot/ROS/rur_detailed_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
